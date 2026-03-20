e7417962dcdcd03f33fa4b3dddba2fe02be5047c
ykyk <yangk7675@sina.com>
2026-03-20 08:55:21 +0800

    keymap.lua

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -24,7 +24,11 @@ keymap.set("n", "s", ":<nop>")
 keymap.set("n", "S", ":w<CR>")
 keymap.set("n", "Q", ":q<CR>")
 keymap.set("n", "<Leader>qq", ":xa!<CR>")
-
+-- 后退（上一个文件）：Ctrl + O
+-- 前进（下一个文件）：Ctrl + I
+-- :new 文件名  " 横向分窗新建文件（如 :new demo.md）
+-- :vnew 文件名 " 纵向分窗新建文件
+-- :edit 文件名 " 不拆分窗口，直接新建/打开文件（最常用）
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
