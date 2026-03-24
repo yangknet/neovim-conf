d8a6ec356fe7611fb4c5f5eaef7a27ae0c41aa33
ykyk <git@yangk.net>
2026-03-24 22:32:00 +0800

    del-im-select

--- nvim/lua/plugins/im-select.lua
+++
@@ -1,8 +0,0 @@
-return { -- mac系统需要安装macism命令
-    "keaising/im-select.nvim",
-    config = function()
-        require("im_select").setup({
-          default_command = "macism"
-        })
-    end,
-}
